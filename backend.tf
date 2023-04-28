569675bcb7b13d1101f6da1a11ca1b80a17822ae
ade-adeniyi <121461784+ade-adeniyi@users.noreply.github.com>
2023-04-28 13:14:08 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "my-aws-tf-state-bucket"
+    bucket = "my-aws-tf-state-demo"
     key = "main"
     region = "us-east-1"
     dynamodb_table = "my-dynamo-db-table"
